f9ad7aca202c59fe488e584562bedc42edd0c65f
Kcoder674 <kvcassaday@gmail.com>
2023-04-30 14:53:06 -0600

    Added a calc

--- maths.py
+++ maths.py
@@ -1,3 +1,11 @@
+"""This is a module that contains math functions"""
+
+
+class Employee:
+    """This is a class that stores details of all of the people who work for an organization"""
+    pass
+
+
 def calculate(x, y):
     """This function calculates the sum of two numbers and gives the result
     efnjdnsin fjnejfns fjensjp"""
@@ -5,3 +13,5 @@ def calculate(x, y):
 
 
 calculate(3, 4)
+calculate(2, 7)
+print(help(calculate))
